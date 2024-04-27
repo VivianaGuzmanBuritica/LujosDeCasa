c801736253fe8bfc658f510c23cc14f9189551f8
vivianaGuzman <vivianaguzmanburitica@gmail.com>
2024-04-26 22:18:52 -0500

    quienesSomos

--- src/about-us/AboutUs.jsx
+++ src/about-us/AboutUs.jsx
@@ -9,25 +9,31 @@ const AboutUs = () => {
   
   return (
     <section id="about-us" className='containerAbout'>
-      
-    <h3 className='titleSection'> {language === 'es' ?  'Quiénes somos' : ' About us' }</h3> 
+          <h3 className='titleSection'> {language === 'es' ?  'Quiénes somos' : ' About us' }</h3> 
+   
     <section className='articles' >
-      <article>
-        <div className='img-logo'>
-          <a href='https://alliancebioversityciat.org/es' target="_blank">
-            {language === 'es' ?  <img className='logoCiat' src="/logos/logoCiatEs.png" alt="logo-ciat" /> : 
-            <img className='logoCiat'  src="/logos/logoCiatEN.png" alt="logo-ciat" /> }
-
-          </a>
+      <article className='quienes'>
+        <div className='img-logo'>         
+            <img className='logoCiat1'  src="/post/1.png" alt="logo-ciat" />
+            <img className='logoCiat'  src="/img/woman-tailor-working-at-the-sewing-factory.jpg" alt="logo-ciat" />
+            <img className='logoCiat'  src="/post/3.png" alt="logo-ciat" />        
+        
         </div>
+        <div className='img-logo'>         
+            <img className='logoCiat'  src="/post/1.png" alt="logo-ciat" />
+            <img className='logoCiat'  src="/img/woman-tailor-working-at-the-sewing-factory.jpg" alt="logo-ciat" />
+            <img className='logoCiat'  src="/post/3.png" alt="logo-ciat" />        
+        
+        </div> 
+        </article>
         <div>
             <p className='text'>
-            {language === 'es' ?  ' La Alianza de Bioversity International y el Centro Internacional de Agricultura Tropical (CIAT) brinda soluciones científicas que aprovechan la biodiversidad agrícola y transforman los sistemas alimentarios de manera sostenible con el objetivo de preservar y proteger el medio ambiente.' : 'The Alliance of Bioversity International and the International Center for Tropical Agriculture (CIAT) provides scientific solutions that take advantage of agricultural biodiversity and transform food systems in a sustainable way in order to protect and preserve the environment.' }
+            {language === 'es' ?  'Lujos de Casa es una empresa con sello colombiano nace para satisfacer las necesidad de la industria hotelera, restaurantes, Cajas de compensación etc. Empleamos los mejores materiales para crear productos fáciles de limpiar, duraderos y amigables con el medio ambiente. Nos apasiona el servicio y brindar  soluciones acorde a las necesidades de cada uno de nuestros clientes.':'' }
            
             </p>
         </div>
-      </article>
-      <article>
+     
+      {/* <article>
       <div className='img-logo'>
       <a href='https://producersdirect.org/' target="_blank"><img className='logoCiat' src="/logos/logoProducersDirect.png" alt="logo-producers-direct" /></a>
         </div>
@@ -45,7 +51,7 @@ const AboutUs = () => {
             {language === 'es' ?  ' Tecnicafé, es una corporación mixta sin animo de lucro que ha sido creada como el Primer Parque Tecnológico abierto y global, ubicado en el departamento del Cauca en Colombia. Con el objetivo de realizar innovación transformativa en el mundo del café a través de la sostenibilidad.' : ' Tecnicafé, a private-public non-profit organization, is the first global coffee technological innovation park, located in the department of Cauca in Colombia. It aims to carry out transformative innovation in the world of coffee through sustainability.' }
            </p>
         </div>
-      </article>
+      </article> */}
     
     </section>
   
